b9227d84e621a3b2466ee5c8e5f31ddf54d2aeb3
abhiachalla <70327235+abhiachalla@users.noreply.github.com>
2024-02-10 20:17:13 -0800

    Added Scheduling logic

--- src/main/java/com/example/demo/services/SchedulerService.java
+++ src/main/java/com/example/demo/services/SchedulerService.java
@@ -1,24 +1,63 @@
 package com.example.demo.services;
 
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import org.springframework.stereotype.Service;
 
 import com.example.demo.models.Customer;
+import com.example.demo.models.CustomerType;
 
 @Service
 public class SchedulerService {
     
-    List<Customer> customerList;
+    Queue<Customer> VIPCustomerList;
+    Queue<Customer> NormalCustomerList;    
+
+    int vipCount;
+    int normalCount;
 
     public SchedulerService() {
-        this.customerList = new ArrayList<>();
+        this.VIPCustomerList = new LinkedList<>();
+        this.NormalCustomerList = new LinkedList<>();
+
+        this.vipCount = 0;
+        this.normalCount = 0;   
     }
 
-    public int addCustomer(Customer customer) {
-        this.customerList.add(customer);
-        return this.customerList.size();
+    public void checkIn(Customer customer) {
+
+        if (customer.getCustomerType().equals(CustomerType.VIP)) {
+            VIPCustomerList.add(customer);
+        } else {
+            NormalCustomerList.add(customer);
+        }
+
+        // return Ticket Here
     }
+
+    public Customer getNextCustomer() {
+
+        if(vipCount < 2 && !VIPCustomerList.isEmpty()) {
+            vipCount++;
+            return VIPCustomerList.poll();
+        } else if(!NormalCustomerList.isEmpty()) {
+            normalCount++;
+            vipCount = 0;
+            normalCount = 0;
+            return NormalCustomerList.poll();
+        }
+
+        return new Customer("", "", CustomerType.EMPTY, "");
+    }
+
 }
+
+
+
+
+
+
+
+// 1.) Scheduler should decide the order of customers to be served.
+// 2.) Employee APIs for serving customers.
